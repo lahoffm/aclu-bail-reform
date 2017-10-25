45c70276343a7109798fde4528fe51164e37bb64
MotorizedWandOfFury <slightlysatirical@gmail.com>
2017-10-24 20:16:33 -0400

    Scrapping some content from page for a single inmate

--- src/webscraper/Richmond/richmond_scrapper.py
+++ src/webscraper/Richmond/richmond_scrapper.py
@@ -3,9 +3,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
 
 driver = webdriver.Chrome()
 driver.get("http://appweb2.augustaga.gov/InmateInquiry/AltInmatesOnline.aspx")
@@ -20,7 +22,7 @@ except NoSuchElementException as error:
     print("Error: {0}".format(error))
     exit()
 
-html = driver.page_source
+html = driver.find_element_by_tag_name("body").get_attribute("innerHTML")
 
 soup = BeautifulSoup(html, "html.parser")
 
@@ -34,19 +36,53 @@ row_link_id = row_link["id"]
 try:
     row_link_elem = driver.find_element_by_id(row_link_id)
     row_link_elem.click()
-
     
 except NoSuchElementException as error:
     print("Error: {0}".format(error))
     exit()
 
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "InmateData1_pnlInmate")))
+booking_number = ""
+full_name = ""
+arrest_date = ""
+race = ""
+sex = ""
+age = ""
+charges = []
+charges_bond = []
+charges_status = []
 
+for attempt in range(0,3):
+    try:
+        booking_number = driver.find_element_by_id("lblBKNo").get_attribute("innerText") 
+        full_name = driver.find_element_by_id("InmateData1_lblFullName").get_attribute("innerText")
+        arrest_date = driver.find_element_by_id("InmateData1_lblArrDt").get_attribute("innerText")
+        race = driver.find_element_by_id("InmateData1_lblRace").get_attribute("innerText")
+        sex = driver.find_element_by_id("InmateData1_lblSex").get_attribute("innerText")
+        age = driver.find_element_by_id("InmateData1_lblAge").get_attribute("innerText")
 
-updated_soup = BeautifulSoup(driver.page_source, "html.parser")
-detail_panel = updated_soup.find(id="pnlDetail")
+        charge_rows = driver.find_elements_by_css_selector("table#InmateData1_dlChgs > tbody > tr")
+        charge_rows_count = len(charge_rows) - 1 #Subtract column header
+        for charge_num in range(0, charge_rows_count):
+            charge_selector = "InmateData1_dlChgs_lblChg_{0}".format(charge_num)
+            charge = driver.find_element_by_id(charge_selector).get_attribute("innerText")
+            print(charge)
+        
+    except StaleElementReferenceException:
+        print("Dom updated, trying again")
+        continue
+    
+    break
+    
 
-print(detail_panel.find("div", id="InmateData1_pnlInmate"))
+county_name = "Richmond"
+timestamp = datetime.now()
+url = "http://appweb2.augustaga.gov/InmateInquiry/AltInmatesOnline.aspx"
+inmate_id = ""
+inmate_lastname = ""
+inmate_firstname = ""
+
+print(booking_number, full_name, arrest_date, race, sex, age)
+
 #test
 
 #parse all rows
@@ -54,4 +90,4 @@ print(detail_panel.find("div", id="InmateData1_pnlInmate"))
 #check if next page link is clickable, if it is, click and parse all the booking rows
 #stop when next link is not clickable
 
-#driver.close()
+driver.close()
