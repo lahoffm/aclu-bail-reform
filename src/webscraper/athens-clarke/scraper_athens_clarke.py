ec3be7352689cf1ca1e0c2c92b9d9dae7a5d0e98
Lukas Hoffmann <lahoffm@gmail.com>
2017-10-31 15:49:25 -0400

    Modify bibb scraper to conform to CONTRIBUTING.md

--- src/webscraper/athens-clarke/scraper_athens_clarke.py
+++ src/webscraper/athens-clarke/scraper_athens_clarke.py
@@ -242,7 +242,8 @@ class ScraperAthensClarke(object):
             #      <a href="##" onclick="window.open('detailsNEW.asp?id=-59091&amp;bid=2017-00004550&amp;pg=1&amp;curr=yes',
             #      'search', 'width=730,height=800,status=yes,resizable=yes,scrollbars=yes')">ABOYADE, OLUFEMI BABALOLA</a>
             i = i + 1
-            print('Downloading subpage {0} of {1}...'.format(i, nlinks))
+            print('Downloading subpage {0} of {1} at {2}...'.format(i, nlinks, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
             url_main = self.url_roster if flag=='roster' else self.url_booking
             subpage_url = url_main[0:url_main.rfind('/')+1] + a["onclick"].split(',')[0].split("'")[1]
             if any(self.df['url'].isin([subpage_url])): # already visited link - happens for booking site
